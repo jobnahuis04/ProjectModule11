6ace02f15d6a91844f4c8af8aaf50726f3bd6f54
jobnahuis04 <jobnahuis@gmail.com>
2026-02-07 21:44:09 +0100

    basis variabelen aan class: OrderedParts toegevoegd

--- Classes_setup.py
+++ Classes_setup.py
@@ -1,19 +1,25 @@
-class OrderedPart:
+class Orders:
     def __init__(self):
-        self.part_id = []
-        self.price = []
-        self.route = []
+        self.order_number = []
         self.status = []
         self.order_date = []
         self.delivery_date = []
         self.quantity = []
-        self.max_transport_batch = []
-        self.max_assembly_batch = []
-        self.setup_time = []
-        self.process_time = []
-        self.stock = []
+
+class OrderedPart:
+    def __init__(self):
+        self.part_id = []               # done
+        self.price = []                 # done
+        self.route = []                 # done
+        self.max_transport_batch = []   # done
+        self.setup_time = []            # done
+        self.process_time = []          # done
+        self.stock_size = []            # done
+
         self.is_main_assembly = True
         self.sub_parts_id = []
+        self.max_assembly_batch = []
+        self.orders = Orders()
 
     class Machine:
         def __init__(self):
@@ -21,4 +27,6 @@ class OrderedPart:
             self.machine_id = []
             self.active_part_id = []
             self.idle_time = []
+            self.purchase_cost = []
+
 
--- make_json_ordered_parts.py
+++ make_json_ordered_parts.py
@@ -4,15 +4,22 @@ import numpy as np
 
 df = pd.read_csv("./data/Product portfolio.csv")
 
-part_numbers = df["Part number"]
 all_routes = df["Machine routing"]
 all_setup_time = df["Setup time (h)"]
 all_process_time = df["Process time (h)"]
+all_max_transport_batch = df["Max transport batch size (pieces) "]
 
-indices_part_numbers = part_numbers.notna()
+part_numbers = df["Part number"]
+stock_size = df["Size (indicative)"]
+price = df["Price per part (€) "]
 
+# start class
 ordered_part = OrderedPart()
+
+# simple parameters
 ordered_part.part_id = part_numbers.dropna().tolist()
+ordered_part.stock_size = stock_size.dropna().tolist()
+ordered_part.prize = price.dropna().tolist()
 
 # finding all rows that correspond to a part
 index_start_part = [None]*len(ordered_part.part_id)
@@ -24,7 +31,7 @@ for i in range(1,len(ordered_part.part_id)):
 index_start_part[-1] = part_numbers[part_numbers == ordered_part.part_id[-1]].index[0]
 index_end_part[-1] = len(part_numbers)-1
 
-# saving route in class
+# saving data in class
 def save_machining_info_to_class(all_data, class_variable):
     for i in range(len(ordered_part.part_id)):
         data = all_data[index_start_part[i]:index_end_part[i]]
@@ -32,3 +39,9 @@ def save_machining_info_to_class(all_data, class_variable):
 save_machining_info_to_class(all_routes, "route")
 save_machining_info_to_class(all_setup_time, "setup_time")
 save_machining_info_to_class(all_process_time, "process_time")
+
+def save_first_attribute_to_class(all_data, class_variable):
+    for i in range(len(ordered_part.part_id)):
+        data = all_data[index_start_part[i]]
+        getattr(ordered_part, class_variable).append(data)
+save_first_attribute_to_class(all_max_transport_batch, "max_transport_batch")
