4059948fffff79762a716cb03e2edabf5e81f913
jobnahuis04 <jobnahuis@gmail.com>
2026-02-12 13:15:09 +0100

    Histogrammen per production line finished

--- plot_different_properties.py
+++ plot_different_properties.py
@@ -1,4 +1,6 @@
 from matplotlib import pyplot as plt
+from pygments.lexers import go
+
 from make_json_ordered_parts import ordered_part
 from plotly.colors import qualitative
 
@@ -41,9 +43,48 @@ def plot_different_properties(properties, units):
     plt.tight_layout()
     plt.show()
 
+def plot_different_production_lines(properties, units):
+    x_property = properties[0]
+    y_property = properties[1]
+
+    x_values = getattr(ordered_part, x_property)
+    y_values = getattr(ordered_part, y_property)
+    route_numbers = ordered_part.route_number
+
+    production_lines_x = [None] * (max(route_numbers)+1)
+    production_lines_y = [None] * (max(route_numbers)+1)
+    route_numbers_2 = range(len(production_lines_x))
+    for i in route_numbers_2:
+        x = []
+        y = []
+        for j in range(len(ordered_part.part_id)):
+            if ordered_part.route_number[j] == i:
+                y.append(y_values[j])
+        production_lines_x[i] = "production line "+str(i)
+        production_lines_y[i] = sum(y)
+
+    color_map = build_route_color_map(route_numbers_2)
+
+    combined = list(zip(route_numbers_2, production_lines_y, production_lines_x))
+
+    # sort by production line
+    combined.sort(reverse=False, key=lambda x: x[0])
+    route_numbers, y_values, x_values = zip(*combined)
+    color_values = [color_map[number] for number in route_numbers]
+
+    plt.figure()
+    plt.bar(x_values, y_values, color=color_values)
+    plt.xlabel("production line [" + units[0] + "]")
+    plt.ylabel(y_property + " [" + units[1] + "]")
+    plt.title(f"{y_property} per production line")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+
 properties = [["part_id", "total_quantity"],
               ["part_id", "total_time_all_parts"]]
 units = [["-", "-"],
          ["-", "hours"]]
 for i, property in enumerate(properties):
     plot_different_properties(property, units[i])
+    plot_different_production_lines(property, units[i])
